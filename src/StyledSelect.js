c77a1621d4fe82b7fa1f597337ff39ce7e189a94
Jim Ji <jimji1005@gmail.com>
2022-01-28 15:15:20 -0800

    fix missing useEffect

--- src/StyledSelect.js
+++ src/StyledSelect.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 
