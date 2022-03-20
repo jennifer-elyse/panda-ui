ce0a7883ed5f7e04bf3b6616d6b5daf0ca6d749f
Jennifer Kirchner <jkirchne@cswg.com>
2022-03-20 18:23:40 -0400

    Working on iOS and Android

--- examples/components/StickyColumnTable.js
+++ examples/components/StickyColumnTable.js
@@ -70,29 +70,29 @@ const StickyColummnTable = (props) => {
 	// This is a hacky way to do this, but we're not sure how to read the correct dimensions
 	// outside of a scroll event.
 	useEffect(() => {
-		headerHorizontalScrollView.current.scrollTo({ x: 1 });
+		// headerHorizontalScrollView.current.scrollTo({ x: 1 });
 	}, []);
 
 	const handleScroll_headerHorizontal = (event) => {
-		activeScroller.current = ScrollerEnum.Header;
-		const positionX = event.nativeEvent.contentOffset.x;
-		const outerWidth = event.nativeEvent.layoutMeasurement.width;
-		const innerWidth = event.nativeEvent.contentSize.width;
-		const positionXRemaining = innerWidth - outerWidth - positionX;
+		// activeScroller.current = ScrollerEnum.Header;
+		// const positionX = event.nativeEvent.contentOffset.x;
+		// const outerWidth = event.nativeEvent.layoutMeasurement.width;
+		// const innerWidth = event.nativeEvent.contentSize.width;
+		// const positionXRemaining = innerWidth - outerWidth - positionX;
 
-		setShowLeftChevron(positionX > 20);
-		setShowRightChevron(positionXRemaining > 20);
+		// setShowLeftChevron(positionX > 20);
+		// setShowRightChevron(positionXRemaining > 20);
 
-		if (Platform.OS === 'ios' || activeScroller.current === ScrollerEnum.Header) {
-			contentHorizontalScrollView.current.scrollTo({ x: positionX, animated: false });
-		}
+		// if (Platform.OS === 'ios' || activeScroller.current === ScrollerEnum.Header) {
+		// 	contentHorizontalScrollView.current.scrollTo({ x: positionX, animated: false });
+		// }
 	};
 
 	const handleScroll_contentHorizontal = (event) => {
-		if (Platform.OS === 'ios' || activeScroller.current === ScrollerEnum.ScrollContent) {
-			const positionX = event.nativeEvent.contentOffset.x;
-			headerHorizontalScrollView.current.scrollTo({ x: positionX, animated: false });
-		}
+		// if (Platform.OS === 'ios' || activeScroller.current === ScrollerEnum.ScrollContent) {
+		// 	const positionX = event.nativeEvent.contentOffset.x;
+		// 	headerHorizontalScrollView.current.scrollTo({ x: positionX, animated: false });
+		// }
 	};
 
 	const handleScroll_stickyContent = (event) => {
@@ -172,113 +172,114 @@ const StickyColummnTable = (props) => {
 
 			<View key="contentColumn" style={{ width: 250 }}>
 				<ScrollView
-					ref={headerHorizontalScrollView}
-					onScroll={handleScroll_headerHorizontal}
-					scrollEventThrottle={16}
-					onScrollBeginDrag={() => activeScroller.current = ScrollerEnum.Header}
-					horizontal={true}
-					fadingEdgeLength={150}
-					contentContainerStyle={{ flexGrow: 1, height: '100%', padding: 0, alignItems: 'space-between' }}
-				>
-					{showLeftChevron &&
-						<Button
-							iconElement={<Icon name="chevron-left" size={14} color={Colors[theme].buttonTextColor} />}
-							size="small"
-							height={40}
-							width={10}
-							solid={false}
-							style={{ marginLeft: 2, justifyContent: 'center' }}
-							disabled
-							border={false}
-							transparent
-							onPress={() => ({}) }
-							key="left-arrow"
-						/>
-					}
-						{columns.map((option, i) => {
-							return (
-								<Button
-									label={option.label}
-									color={Colors[theme].tintColor}
-									textColor={Colors[theme].buttonTextColor}
-									// size={size}
-									height={40}
-									// disabled={disabled}
-									width={i === 2 || i === 4 ? 200 : 100}
-									border={false}
-									// color={selected ? color : inactiveTextColor}
-									// textColor={selected ? activeTextColor : inactiveTextColor}
-									// style={i === 0
-									// 	? selected ? buttonSelectedLeftStyle : buttonLeftStyle
-									// 	: selected ? buttonSelectedNotLeftStyle : buttonNotLeftStyle}
-									onPress={() => ({})}
-									key={option.key}
-									gradient={[]}
-								/>
-							);
-						})}
-
-
-					{showRightChevron &&
-						<Button
-							iconElement={<Icon name="chevron-right" size={14} color={Colors[theme].buttonTextColor} />}
-							size="small"
-							height={40}
-							width={10}
-							solid={false}
-							style={{ marginRight: 2, justifyContent: 'center' }}
-							transparent
-							disabled
-							border={false}
-							onPress={() => ({}) }
-							key="right-arrow"
-						/>
-					}
-				</ScrollView>
-				<ScrollView
 					ref={contentHorizontalScrollView}
 					scrollEventThrottle={16}
 					horizontal
 					onScroll={handleScroll_contentHorizontal}
 					onScrollBeginDrag={() => activeScroller.current = ScrollerEnum.ScrollContent}
-					fadingEdgeLength={150}
 					indicatorStyle="black"
 					persistentScrollbar
 				>
-					<ScrollView
-						ref={contentVerticalScrollView}
-						onScroll={handleScroll_contentVertical}
-						onScrollBeginDrag={() => activeScroller.current = ScrollerEnum.ScrollContent}
-						scrollEventThrottle={16}
-						persistentScrollbar
-					>
-						<View>
-							{ 	data.map((item, i) => {
+					<View>
+						<View
+							// ref={headerHorizontalScrollView}
+							// onScroll={handleScroll_headerHorizontal}
+							// scrollEventThrottle={16}
+							// onScrollBeginDrag={() => activeScroller.current = ScrollerEnum.Header}
+							// horizontal
+							// contentContainerS
+							style={{ padding: 0, alignItems: 'space-between', flexDirection: 'row' }}
+						>
+							{showLeftChevron &&
+								<Button
+									iconElement={<Icon name="chevron-left" size={14} color={Colors[theme].buttonTextColor} />}
+									size="small"
+									height={40}
+									width={10}
+									solid={false}
+									style={{ marginLeft: 2, justifyContent: 'center' }}
+									disabled
+									border={false}
+									transparent
+									onPress={() => ({}) }
+									key="left-arrow"
+								/>
+							}
+								{columns.map((option, i) => {
 									return (
-										<View
-											key={item.characterId}
-											style={{ flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-											<View style={{ width: 100, height: 25, alignItems: 'center' }}>
-												<Text>{item.name}</Text>
-											</View>
-											<View style={{ width: 100, height: 25, alignItems: 'center' }}>
-												<Text>{item.color}</Text>
-											</View>
-											<View style={{ width: 200, height: 25, alignItems: 'center' }}>
-												<Text>{item.faveFood}</Text>
-											</View>
-											<View style={{ width: 100, height: 25, alignItems: 'center' }}>
-												<Text>{item.peeves}</Text>
-											</View>
-											<View style={{ width: 200, height: 25, alignItems: 'center' }}>
-												<Text>{item.loves}</Text>
-											</View>
-										</View>
-									)
-								})
+										<Button
+											label={option.label}
+											color={Colors[theme].tintColor}
+											textColor={Colors[theme].buttonTextColor}
+											// size={size}
+											height={40}
+											// disabled={disabled}
+											width={i === 2 || i === 4 ? 200 : 100}
+											border={false}
+											// color={selected ? color : inactiveTextColor}
+											// textColor={selected ? activeTextColor : inactiveTextColor}
+											// style={i === 0
+											// 	? selected ? buttonSelectedLeftStyle : buttonLeftStyle
+											// 	: selected ? buttonSelectedNotLeftStyle : buttonNotLeftStyle}
+											onPress={() => ({})}
+											key={option.key}
+											gradient={[]}
+										/>
+									);
+								})}
+
+
+							{showRightChevron &&
+								<Button
+									iconElement={<Icon name="chevron-right" size={14} color={Colors[theme].buttonTextColor} />}
+									size="small"
+									height={40}
+									width={10}
+									solid={false}
+									style={{ marginRight: 2, justifyContent: 'center' }}
+									transparent
+									disabled
+									border={false}
+									onPress={() => ({}) }
+									key="right-arrow"
+								/>
 							}
 						</View>
-					</ScrollView>
+						<ScrollView
+							ref={contentVerticalScrollView}
+							onScroll={handleScroll_contentVertical}
+							onScrollBeginDrag={() => activeScroller.current = ScrollerEnum.ScrollContent}
+							scrollEventThrottle={16}
+							persistentScrollbar
+						>
+							<View>
+								{ 	data.map((item, i) => {
+										return (
+											<View
+												key={item.characterId}
+												style={{ flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+												<View style={{ width: 100, height: 25, alignItems: 'center' }}>
+													<Text>{item.name}</Text>
+												</View>
+												<View style={{ width: 100, height: 25, alignItems: 'center' }}>
+													<Text>{item.color}</Text>
+												</View>
+												<View style={{ width: 200, height: 25, alignItems: 'center' }}>
+													<Text>{item.faveFood}</Text>
+												</View>
+												<View style={{ width: 100, height: 25, alignItems: 'center' }}>
+													<Text>{item.peeves}</Text>
+												</View>
+												<View style={{ width: 200, height: 25, alignItems: 'center' }}>
+													<Text>{item.loves}</Text>
+												</View>
+											</View>
+										)
+									})
+								}
+							</View>
+						</ScrollView>
+					</View>
 				</ScrollView>
 			</View>
 		</View>
